5c622a60ccafcca77d1dccc9637e16c876799ef7
Christopher Alphonse <christopheralphonse96.com>
2022-08-20 19:07:13 -0400

    first commit

---
+++ Chaining If Else Statements/Chaining-If-Else-Statements.js
@@ -0,0 +1,17 @@
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  } else {
+    return "Change Me";
+  }
+}
+testSize(7);
+console.log(testSize(7));
