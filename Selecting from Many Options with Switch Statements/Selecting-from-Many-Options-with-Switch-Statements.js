36b99cd7ab799c0594509f957c6745ea0b1e69d9
Christopher Alphonse <christopheralphonse96.com>
2022-08-21 15:35:16 -0400

    new folder

---
+++ Selecting from Many Options with Switch Statements/Selecting-from-Many-Options-with-Switch-Statements.js
@@ -0,0 +1,10 @@
+const caseInSwitch = (val) => {
+  let answer = "";
+  // Only change code below this line
+
+  // Only change code above this line
+  return answer;
+};
+
+caseInSwitch(1);
+console.log(caseInSwitch(1));
