f35b74f1587fd6d4d7a361bc829ec92bf7377dfb
Christopher Alphonse <christopheralphonse96.com>
2022-08-21 15:50:48 -0400

    cc

--- Selecting from Many Options with Switch Statements/Selecting-from-Many-Options-with-Switch-Statements.js
+++ Selecting from Many Options with Switch Statements/Selecting-from-Many-Options-with-Switch-Statements.js
@@ -1,10 +1,27 @@
 const caseInSwitch = (val) => {
   let answer = "";
   // Only change code below this line
-
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+  }
   // Only change code above this line
   return answer;
 };
 
 caseInSwitch(1);
-console.log(caseInSwitch(1));
+console.log(caseInSwitch(4));
+
+// 1 - alpha
+// 2 - beta
+// 3 - gamma
+// 4 - delta
