b9e95051f966b3fdee6a803fee4cc4839430dbce
Christopher Alphonse <christopheralphonse96.com>
2022-08-21 16:02:20 -0400

    no changes

---
+++ Adding a Default Option in Switch Statements/Adding-a-Default-Option-in-Switch-Statements.js
@@ -0,0 +1,11 @@
+var store = ["a - apple", "b - bird", "c - cat", "default - stuff "];
+
+const switchOfStuff = (val) => {
+  let answer = "";
+  // Only change code below this line
+  console.log(store[0]);
+  // Only change code above this line
+  return answer;
+};
+
+switchOfStuff(1);
