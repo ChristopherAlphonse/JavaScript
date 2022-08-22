f6a104e765749386157e125f59bb988d35557566
Christopher Alphonse <christopheralphonse96.com>
2022-08-21 22:13:07 -0400

    done

---
+++ Replacing If Else Chains with Switch/Replacing-If-Else-Chains-with-Switch.js
@@ -0,0 +1,21 @@
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
