ecbca193dba537093a511083262f944d05b930aa
Christopher Alphonse <christopheralphonse96@gmail.com>
2023-01-18 19:52:30 -0500

    done with this for now

--- Udemy/test.js
+++ Udemy/test.js
@@ -1,11 +1,9 @@
-const car = ["y", "ca", "car"];
+const car = ["car"];
 
 const findCar = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "car") {
       console.log(`found the ${arr[i]}`);
-    } else {
-      console.log("Not Found");
     }
   }
 };
