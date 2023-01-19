dc7d4df6a0031dfc5d31074ca5025b659767daab
Christopher Alphonse <christopheralphonse96@gmail.com>
2023-01-18 19:50:19 -0500

    test done

--- Udemy/test.js
+++ Udemy/test.js
@@ -1,1 +1,13 @@
-console.log("test");
+const car = ["y", "ca", "car"];
+
+const findCar = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "car") {
+      console.log(`found the ${arr[i]}`);
+    } else {
+      console.log("Not Found");
+    }
+  }
+};
+
+findCar(car);
