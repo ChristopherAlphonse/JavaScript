be3de2fb75a53a854ce432292a857bb2b6a3782b
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-11-17 01:47:22 -0500

    challenge

--- DSA Code/findTheSquareRoot.js
+++ DSA Code/findTheSquareRoot.js
@@ -1,30 +1,62 @@
 function same(n, target) {
-	// Check if arrays are of equal length
 	if (n.length !== target.length) {
 		return false;
 	}
 
-	// Loop through the first array
 	for (let i = 0; i < n.length; i++) {
-		// Check if the square of the current element in n exists in target
 		const squared = Math.pow(n[i], 2);
-
 		const val = target.indexOf(squared);
 
-		// If the square is not found in target, return false
 		if (val === -1) {
 			return false;
 		}
 
-		// Remove the found element from target to avoid duplicate matches
 		target.splice(val, 1);
 	}
 
-	// If all elements have been matched, return true
 	return true;
 }
-let print = console.log;
 
-print(same([1, 2, 3], [4, 1, 9])); // Should return true
-print(same([1, 2, 3], [1, 9])); // Should return false
-print(same([1, 2, 1], [4, 4, 1])); // should return false (must be the same frequency)
+console.log(same([1, 2, 3], [4, 1, 9])); // Should return true
+console.log(same([1, 2, 3], [1, 9])); // Should return false
+console.log(same([1, 2, 1], [4, 4, 1])); // Should return false (must be the same frequency)
+
+/*
+Time: 0(n^2)
+Space: 0(1)
+*/
+
+// hashMap solution:
+
+// refactored
+const Same = (arr1, arr2) => {
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	let freqCounter = {};
+	let freqCounter2 = {};
+	for (let i of arr1) {
+		freqCounter[i] = (freqCounter[i] || 0) + 1;
+	}
+	for (let j of arr2) {
+		freqCounter2[j] = (freqCounter2[j] || 0) + 1;
+	}
+
+	for (let key in freqCounter) {
+		let squaredKey = key ** 2;
+		if (!(squaredKey in freqCounter2)) {
+			return false;
+		}
+		if (freqCounter2[squaredKey] !== freqCounter[key]) {
+			return false;
+		}
+	}
+
+	return true;
+};
+
+/*
+Time: 0(n)
+Space: 0(n)
+*/
