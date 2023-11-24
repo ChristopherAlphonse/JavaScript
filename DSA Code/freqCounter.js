601599cba7060e06cfa540298d95a5c043a28d03
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-11-24 03:49:24 -0500

    test: fix

--- DSA Code/freqCounter.js
+++ DSA Code/freqCounter.js
@@ -4,28 +4,28 @@ function freq(arr1, arr2) {
   let freq1 = {};
   let freq2 = {};
 
-  for (let index in arr1) {
-    freq1[index] = (freq1[index] || 0) + 1;
+  for (let index of arr1) {
+    freq1[index] = freq1[index] + 0 || +1;
     // console.log(freq1);
   }
-  for (let index in arr2) {
-    freq2[index] = (freq2[index] || 0) + 1;
+  for (let index of arr2) {
+    freq2[index] = freq2[index] + 0 || +1;
     // console.log(freq2);
   }
 
   for (let key in freq1) {
-    if (!(key ** 2 in freq2)) {
+    if (!(Math.pow(parseInt(key), 2) in freq2)) {
       return false;
     }
     if (freq2[key ** 2] !== freq1[key]) {
       return false;
     }
-    console.log(`freq1 ${freq1[key]} | freq2:${freq2[key]} | key: ${key}`);
+    console.log(`freq1 ${freq1[key]} | freq2:${freq2[key ** 2]} | key: ${key}`);
   }
 
   return true;
 }
 
-// console.log(freq([1, 3], [4, 4, 1])); //false
+console.log(freq([1, 3], [4, 4, 1])); //false
 console.log(freq([1, 2, 3], [1, 4, 9])); //true
-// console.log(freq([1, 2, 3], [4, 4, 1])); //false
+console.log(freq([1, 2, 3], [4, 4, 1])); //false
