416e079ec853e759c2a51a7241df4631d8b76039
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-12-08 03:10:19 -0500

    testing bug

--- DSA Code/findThePairs.js
+++ DSA Code/findThePairs.js
@@ -3,6 +3,7 @@ const arr1 = [0, 1, 2, 0, 1, 2];
 function printAllPairs(n) {
   let pairs = [];
   let left = n.length - 1;
+
   for (let i = 0; i < left; i++) {
     for (let j = i + 1; j < n.length; j++) {
       pairs.push([n[i], n[j]]);
