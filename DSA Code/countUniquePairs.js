236a0534f967537a6e08173d6d9871ae07061e21
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-12-09 15:52:52 -0500

    maxsub

--- DSA Code/countUniquePairs.js
+++ DSA Code/countUniquePairs.js
@@ -2,6 +2,9 @@
 Implement a function called countUniqueValues, which accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it will always be sorted
   */
 
+
+// two pointers pattern
+
 function countUniqueValues(arr) {
   if (arr.length === 0) return 0;
 
