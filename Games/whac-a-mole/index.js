2f80626cb53016d5f99942d7602ee22ff8d74700
ChristopherAlphonse <christopheralphonse96@gmail.com>
2023-12-16 21:13:59 -0500

    finish game

--- Games/whac-a-mole/index.js
+++ Games/whac-a-mole/index.js
@@ -1,14 +1,78 @@
-const titles = new Number(9);
+let currMoleTile;
+let currPlantTile;
+let score = 0;
+let gameOver = false;
+let tiles = Number(9);
 
-// window.onload = function () {
-//   setGame();
-// };
+window.onload = function () {
+  setGame();
+};
 
 function setGame() {
-  for (let i = 0; i < titles; i++) {
+  for (let i = 0; i < tiles; i++) {
     let gameTile = document.createElement("div");
-    //  console.log(i);
+    gameTile.id = i.toString();
+    gameTile.addEventListener("click", selectTile);
+    document.getElementById("board").appendChild(gameTile);
+  }
+  setInterval(setMole, 1000);
+  setInterval(setPlant, 1500);
+}
+
+function getRandomTile() {
+  let num = Math.floor(Math.random() * tiles);
+  return String(num);
+}
+
+function setMole() {
+  if (gameOver) {
+    return;
+  }
+  if (currMoleTile) {
+    currMoleTile.innerHTML = "";
+  }
+  let mole = document.createElement("img");
+  mole.src = "./img/monty-mole.png";
+
+  let num = getRandomTile();
+  if (currPlantTile && currPlantTile.id == num) {
+    return;
+  }
+  currMoleTile = document.getElementById(num);
+  currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+  if (gameOver) {
+    return;
+  }
+  if (currPlantTile) {
+    currPlantTile.innerHTML = "";
+  }
+  let plant = document.createElement("img");
+  plant.src = "./img/piranha-plant.png";
+
+  let num = getRandomTile();
+  if (currMoleTile && currMoleTile.id == num) {
+    return;
+  }
+  currPlantTile = document.getElementById(num);
+  currPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+  if (gameOver) {
+    return;
+  }
+  if (this == currMoleTile) {
+    score += 10;
+    document.getElementById("score").innerText = String(score);
+  } else if (this == currPlantTile) {
+    document.getElementById("score").innerText = "GAME OVER: " + String(score);
+    gameOver = true;
   }
 }
 
-setGame();
+function restartGame() {
+  location.reload();
+}
