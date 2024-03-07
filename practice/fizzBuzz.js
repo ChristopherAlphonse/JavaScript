f45e854bb80d7c158b1335539c674476074956fe
christopheralphonse <christopheralphonse96@gmail.com>
2024-03-07 11:16:22 -0500

    g

--- practice/fizzBuzz.js
+++ practice/fizzBuzz.js
@@ -7,18 +7,19 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 */
 
 function FizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    if (i % 15 == 0) {
-      console.log("FizzBuzz " + i);
-    } else if (i % 3 == 0) {
-      console.log("Buzz" + i);
-    } else if (i % 5 == 0) {
-      console.log("Fizz" + i);
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 == 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 == 0) {
+            console.log("Fizz");
+        } else if (i % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
     }
-    console.log(i);
-  }
 }
 
-FizzBuzz(100);
+FizzBuzz(35);
 
 // Time: O(n), space: O(1)
